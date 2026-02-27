5db5f2294ae4fbe7c6e0502d757d5aeb429e6968
JimHagan <jimhagan777@gmail.com>
2026-02-27 12:32:30 -0500

    Some additional changes

--- log_server.py
+++ log_server.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import requests
 import glob
-import google.generativeai as genai
 import json
 
 # Initialize the Flask app
